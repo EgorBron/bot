c2960e19d767f2c467d5a49413ec6be0a9d6c7b9
EgorBron <71507444+EgorBron@users.noreply.github.com>
2021-02-18 19:54:32 +0300

    Create players.py

---
+++ players.py
@@ -0,0 +1,66 @@
+
+player_jester = {
+	'typeof': 'jester',
+	'money': 0,
+	'alive': True,
+	'answered': None
+}
+
+player_believer = {
+	'typeof': 'believer',
+	'money': 0,
+	'alive': True,
+	'answered': None
+}
+
+player_sheriff = {
+	'typeof': 'sheriff',
+	'money': 0,
+	'alive': True,
+	'answered': None
+}
+
+player_screamer = {
+	'typeof': 'screamer',
+	'money': 0,
+	'alive': True,
+	'answered': None
+}
+
+player_red_herring = {
+	'typeof': 'red_herring',
+	'money': 0,
+	'alive': True,
+	'answered': None
+}
+
+player_lovers = {
+	'typeof': 'lovers',
+	'money': 0,
+	'alive': True,
+	'answered': None
+}
+
+player_alpha = {
+	'typeof': 'alpha',
+	'money': 0,
+	'alive': True,
+	'answered': None
+}
+
+player_nerd = {
+	'typeof': 'nerd',
+	'money': 0,
+	'alive': True,
+	'answered': None
+}
+
+audience = {
+	'typeof': 'audience',
+	'money': 0,
+	'alive': True,
+	'quantity': 0,
+	'true_perecent': 0.00
+}
+
+typeplayers = [player_jester, player_believer, player_sheriff, player_screamer, player_red_herring, player_lovers, player_alpha, player_nerd, audience]
